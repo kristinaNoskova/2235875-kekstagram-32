c63d5f6c80278c54ff19ef15e80442e0da544506
Kristina Noskova <krisveronika282@gmail.com>
2024-06-28 13:37:27 +0300

    fix: name changes

--- js/functions.js
+++ js/functions.js
@@ -6,14 +6,29 @@ checkLength('проверяемая строка', 20);
 
 
 function isPalindrome(string) {
-  const normalize = string.replaceAll(' ', '').toLowerCase();
+  const normalization = string.replaceAll(' ', '').toLowerCase();
   let newString = '';
 
-  for (let i = normalize.length - 1; i >= 0; i--) {
-    newString += normalize[i];
+  for (let i = normalization.length - 1; i >= 0; i--) {
+    newString += normalization[i];
   }
 
-  return normalize === newString;
+  return normalization === newString;
 }
 
 isPalindrome('Лёша на полке клопа нашёл');
+
+function returnNumber(string) {
+  const toString = string.toString();
+  let number = '';
+  for (let i = 0; i <= toString.length; i++) {
+    if (!Number.isNaN(parseInt(toString[i], 10))) {
+      number += toString[i];
+    }
+  }
+  return parseInt(number, 10);
+}
+
+returnNumber('1 кефир, 0.5 батона 7');
+
+
