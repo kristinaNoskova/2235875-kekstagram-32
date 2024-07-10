7f371255050d5d980ed4d9a654fa4be9a5729507
Kristina Noskova <krisveronika282@gmail.com>
2024-07-10 15:26:21 +0300

    fix: function of array reshuffling and uniqueness checking has been removed

--- js/main.js
+++ js/main.js
@@ -1,93 +1,58 @@
-function getRandomInteger(min, max) {
+const DESCR_PHOTOS = ['Город', 'cад', 'горы', 'озеро', 'река', 'лес'];
+const NAMES = ['Иван', 'Анна', 'Сергей', 'Елена', 'Максим', 'Ольга', 'Дмитрий', 'Татьяна', 'Александр', 'Мария', 'Андрей', 'Наталья', 'Алексей', 'Ирина', 'Юлия'];
+const TEXT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо.', 'Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.', 'В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+
+
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
-function getUniqRandomGenerator(min, max) {
-  const generatedValues = [];
-
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    for (let i = min; i <= max; i++) {
-      if (generatedValues.includes(currentValue)) {
-        currentValue = getRandomInteger(min, max);
-      } else {
-        break;
-      }
-    }
-    generatedValues.push(currentValue);
-    return currentValue;
-  };
-}
-
-const getLikesCount = getUniqRandomGenerator(15, 200);
-const avatar = getUniqRandomGenerator(1, 6);
-const commentId = getUniqRandomGenerator(0, 200);
-
-function createSerialNumber(min, max) {
+const createSerialNumber = () => {
   let serialNumber = 0;
 
-  return function () {
-    if (serialNumber <= max) {
-      serialNumber += 1;
-    }
+  return () => {
+    serialNumber += 1;
     return serialNumber;
   };
-}
+};
 
-const getCurrentNumber = createSerialNumber(1, 25);
-const getCurrenUrlId = createSerialNumber(1, 25);
-
-
-function getUrl() {
-  return `photos/${getCurrenUrlId()}.jpg`;
-}
-
-function getAvatar() {
-  return `img/avatar-${avatar()}.svg`;
-}
-
-const descrPhotos = ['Город', 'cад', 'горы', 'озеро', 'река', 'лес'];
-const names = ['Иван', 'Анна', 'Сергей', 'Елена', 'Максим', 'Ольга', 'Дмитрий', 'Татьяна', 'Александр', 'Мария', 'Андрей', 'Наталья', 'Алексей', 'Ирина', 'Юлия'];
-const textMessages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают.', 'Как можно было поймать такой неудачный момент?!'];
+const getCurrentNumber = createSerialNumber();
+const getIdPhotos = createSerialNumber();
+const getIdComment = createSerialNumber();
 
 
-function getArrayElement(array) {
-  const randomItems = array[getRandomInteger(0, array.length)];
-  return randomItems;
+const getUrlPhotos = () => `photos/${getIdPhotos()}.jpg`;
+
+const getUrlAvatar = () => `img/avatar-${getRandomInteger(1, 6)}.svg`;
+
+function getArrayElement(elements) {
+  const randomItem = elements[getRandomInteger(0, elements.length - 1)];
+  return randomItem;
 }
 
 const describesComment = () => ({
   return: {
-    id: commentId(),
-    avatar: getAvatar(),
-    message: getArrayElement(textMessages),
-    name: getArrayElement(names)
+    id: getIdComment(),
+    avatar: getUrlAvatar(),
+    message: Array.from({ length: getRandomInteger(1, 2) }, () => getArrayElement(TEXT_MESSAGES)),
+    name: getArrayElement(NAMES)
   }
 });
-
-const arrayWithComments = Array.from({ length: 30 }, describesComment);
-
-function getRandomComment(array) {
-  const mixedArray = array.sort(() => Math.random() - 0.5);
-  const randomNumber = getRandomInteger(0, 30);
-  const randomNumberArray = mixedArray.slice(randomNumber);
-  return randomNumberArray;
-}
 
 const describesPhoto = () => ({
   return: {
     id: getCurrentNumber(),
-    url: getUrl(),
-    description: getArrayElement(descrPhotos),
-    likes: getLikesCount(),
-    comments: getRandomComment(arrayWithComments),
+    url: getUrlPhotos(),
+    description: getArrayElement(DESCR_PHOTOS),
+    likes: getRandomInteger(15, 200),
+    comments: Array.from({ length: getRandomInteger(0, 30) }, describesComment),
   }
 });
 
-const arrayWithPhotos = Array.from({ length: 25 }, describesPhoto);
+const arrayWithPhotos = () => Array.from({ length: 25 }, describesPhoto);
 
-console.log(arrayWithPhotos);
+console.log(arrayWithPhotos());
