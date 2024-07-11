0af41665d224f8e2f969b1b434bc0d648ca9a436
Kristina Noskova <krisveronika282@gmail.com>
2024-07-11 16:21:44 +0300

    feat: function calculates duration meeting

--- js/functions.js
+++ js/functions.js
@@ -4,7 +4,6 @@ function checkLength(string, length) {
 
 checkLength('проверяемая строка', 20);
 
-
 function isPalindrome(string) {
   const normalization = string.replaceAll(' ', '').toLowerCase();
   let newString = '';
@@ -32,3 +31,32 @@ function returnNumber(string) {
 returnNumber('1 кефир, 0.5 батона 7');
 
 
+// ДЗ: 5.16. Функции возвращаются
+
+const convertToTime = (timeString) => {
+  const timeParts = timeString.split(':');
+  const hours = parseInt(timeParts[0], 10);
+  const minutes = parseInt(timeParts[1], 10);
+
+  const date = new Date();
+  date.setHours(hours);
+  date.setMinutes(minutes);
+  return date.toLocaleTimeString().slice(0, 5);
+};
+
+const calculatesDurationMeeting = (startWorkingDay, endWorkingDay, startMeeting, durationMeeting) => {
+  const date = new Date(`2024-07-09T${convertToTime(startMeeting)}`);
+  let endMeetingTime = date.setMinutes(date.getMinutes() + durationMeeting);
+  endMeetingTime = date.toLocaleTimeString().slice(0, 5);
+
+  if (endMeetingTime <= convertToTime(endWorkingDay) && startMeeting >= convertToTime(startWorkingDay)) {
+    return true;
+  }
+  return false;
+};
+
+console.log('1', calculatesDurationMeeting('08:00', '17:30', '14:00', 90)); // true
+console.log('2', calculatesDurationMeeting('8:0', '10:0', '07:00', 120)); // false
+console.log('2', calculatesDurationMeeting('08:00', '14:30', '14:00', 90)); // false
+console.log('4', calculatesDurationMeeting('08:00', '14:30', '14:00', 90)); // false
+console.log('5', calculatesDurationMeeting('8:00', '17:30', '8:0', 900)); // false
