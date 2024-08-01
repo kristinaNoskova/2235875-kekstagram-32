0169469da7cafe554c94b75391d093a9cf9b9f4e
Kristina Noskova <krisveronika282@gmail.com>
2024-08-01 20:54:18 +0300

    fix: variable and function names

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,7 +1,12 @@
 import { isEscapeKey } from './util.js';
 
-const HASHTAG = /^#[a-zя-яё0-9]{1,19}$/i;
+const HASHTAG_REGXP = /^#[a-zя-яё0-9]{1,19}$/i;
 const HASHTAG_COUNT = 5;
+const TextError = {
+  MAX_LENGTH: 'Максимум 5 хештегов',
+  INVALID: 'Хештег должен начинаться с # и включать буквы и цифры',
+  REPEAT: 'Такой хештег уже существует'
+};
 
 const picturesListElement = document.querySelector('.pictures');
 const imgFormElement = picturesListElement.querySelector('.img-upload__form');
@@ -10,11 +15,6 @@ const imgInputElement = imgFormElement.querySelector('.img-upload__input');
 const imgCancelButtonElement = imgFormElement.querySelector('.img-upload__cancel');
 const textCommentsElement = imgFormElement.querySelector('.text__description');
 const textHashtagsElement = imgFormElement.querySelector('.text__hashtags');
-const TextError = {
-  ERROR_LENGTH: 'Максимум 5 хештегов',
-  ERROR_INVALID: 'Хештег должен начинаться с # и включать буквы и цифры',
-  ERROR_REPEAT: 'Такой хештег уже существует'
-};
 
 imgFormElement.method = 'POST';
 imgFormElement.enctype = 'multipart/form-data';
@@ -30,37 +30,33 @@ const pristine = new Pristine(imgFormElement, {
 
 const validateComments = (value) => value.length <= 140;
 
-const normalize = (element) => element.trim().split(' ');
+const getNormalizedHashtags = (element) => element.trim().split(' ');
 
 // Проверяем на соответствие регулярному выражению
 const validateHashtags = (value) => {
-  const arrTags = normalize(value);
-  return arrTags.every((element) => HASHTAG.test(element));
+  const arrTags = getNormalizedHashtags(value);
+
+  return arrTags.every((element) => HASHTAG_REGXP.test(element));
 };
 
-// Проверяем длину массива
-const checksLengthArray = (value) => {
-  const arrTags = normalize(value);
+// Проверяем чтоб длина масиива не привышала HASHTAG_COUNT
+const checkLengthHashtags = (value) => {
+  const arrTags = getNormalizedHashtags(value);
 
-  if (arrTags.length <= HASHTAG_COUNT) {
-    return arrTags.length <= HASHTAG_COUNT;
-  }
+  return arrTags.length <= HASHTAG_COUNT;
 };
 
 // Проверяем не повторяется ли теги
-const checksRepeatArray = (value) => {
-  const arrTags = normalize(value);
+const checkRepeatHashtags = (value) => {
+  const arrTags = getNormalizedHashtags(value);
   const sortedArr = arrTags.slice().sort();
-  if (sortedArr[0] === sortedArr[1]) {
-    return false;
-  }
-  return true;
+  return sortedArr[0] !== sortedArr[1];
 };
 
 pristine.addValidator(textCommentsElement, validateComments, 'Максимум 140 символов');
-pristine.addValidator(textHashtagsElement, validateHashtags, TextError.ERROR_INVALID, 1, true);
-pristine.addValidator(textHashtagsElement, checksLengthArray, TextError.ERROR_LENGTH, 3, true);
-pristine.addValidator(textHashtagsElement, checksRepeatArray, TextError.ERROR_REPEAT, 2, true);
+pristine.addValidator(textHashtagsElement, validateHashtags, TextError.INVALID, 1, true);
+pristine.addValidator(textHashtagsElement, checkLengthHashtags, TextError.MAX_LENGTH, 3, true);
+pristine.addValidator(textHashtagsElement, checkRepeatHashtags, TextError.REPEAT, 2, true);
 
 // Если валидация проходит, разрешаем отправку формы
 imgFormElement.addEventListener('submit', (evt) => {
@@ -70,9 +66,11 @@ imgFormElement.addEventListener('submit', (evt) => {
   }
 });
 
+const isFieldFocused = () => document.activeElement === textCommentsElement || document.activeElement === textHashtagsElement;
+
 // Обработчики для открытия/закрытия формы
 const onDocumentKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
+  if (isEscapeKey(evt) && !isFieldFocused()) {
     evt.preventDefault();
     onUploadOverlayClose();
   }
@@ -96,14 +94,3 @@ function onUploadOverlayClose() {
 imgInputElement.addEventListener('change', onUploadOverlayChange);
 
 imgCancelButtonElement.addEventListener('click', onUploadOverlayClose);
-
-// Потеря фокуса у инпута без закрытия окна
-const onInputKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    evt.target.blur();
-  }
-};
-
-imgFormElement.addEventListener('keydown', onInputKeydown);
