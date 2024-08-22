932e4c8b4cc4d58cceed8ea3415134137bc1f969
Kristina Noskova <krisveronika282@gmail.com>
2024-08-22 15:59:18 +0300

    chenge: server request

--- js/api.js
+++ js/api.js
@@ -9,18 +9,29 @@ const Method = {
   POST: 'POST',
 };
 
-const load = (route, method = Method.GET, body = null) =>
-  fetch(`${BASE_URL}${route}`, { method, body })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`${response.status}: ${response.statusText}`);
-      }
+const load = async (route, method = Method.GET, body = null) => {
+  let response;
+  try {
+    response = await fetch(`${BASE_URL}${route}`, { method, body });
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+  return await response.json();
+};
+// fetch(`${BASE_URL}${route}`, { method, body })
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`${response.status}: ${response.statusText}`);
+//     }
 
-      return response.json();
-    })
-    .catch((err) => {
-      throw new Error(err.message);
-    });
+//     return response.json();
+//   })
+//   .catch((err) => {
+//     throw new Error(err.message);
+//   });
 
 const getData = () => load(Route.GET_DATA);
 
